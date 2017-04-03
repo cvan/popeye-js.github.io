a728c36771c2c6a1c4755f267ced499cebd2d394
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-04-03 01:30:51 -0700

    add `SETTINGS`, `URLS`; clean up speech/app code

--- js/speech.js
+++ js/speech.js
@@ -1,19 +1,56 @@
-/* global annyang */
+/* global annyang, voiceUI */
 
 (function () {
-  var hash = (function (a) {
-    if (a === '') {
-      return {};
-    }
-    var b = {};
-    for (var i = 0; i < a.length; ++i) {
-      var p = a[i].split('=', 2);
-      b[p[0]] = p.length === 1 ? '' : window.decodeURIComponent(p[1].replace(/\+/g, ' '));
-    }
-    return b;
-  })(window.location.hash.substr(1).split('&'));
+  var IS_PROD = !window.location.port && window.location.protocol === 'https:';
 
-  function xhr(opts) {
+  var SETTINGS = {
+    dev: !IS_PROD,
+    prod: IS_PROD,
+    env: IS_PROD ? 'production' : 'development',
+    putio: {
+      clientId: 2801
+    }
+  };
+
+  var URLS = {
+    base: SETTINGS.prod ? 'https://popeye-api.herokuapp.com' : 'http://localhost:7001',
+    putioBase: 'https://api.put.io/v2'
+  };
+  URLS.latestEpisode = function (id) { return `${URLS.base}/latestEpisode?show=${id}`; };
+  URLS.putioTransfersAdd = function (accessToken) { return `${URLS.putioBase}/transfers/add?oauth_token=${accessToken}`; };
+
+  // Generate the URL for the user to log in to Put.io and get redirected back to this page.
+  var redirectUri = encodeURIComponent(window.location.href);
+  var nextUri = encodeURIComponent(`${URLS.putioBase}/oauth2/authenticate?client_id=${SETTINGS.putio.clientId}&response_type=token&redirect_uri=${redirectUri}`);
+  URLS.putioLogin = `${URLS.putioBase}/oauth2/login?next=${nextUri}`;
+
+  var URLHashParams = function (hash) {
+    hash = (hash || window.location.hash.substr(1)).toString().trim();
+    var hashObj = {};
+    if (hash) {
+      var hashPairs = hash.split('&');
+      for (var i = 0; i < hashPairs.length; ++i) {
+        var hashChunks = hash[i].split('=', 2);
+        hashObj[hashChunks[0]] = hashChunks.length === 1 ? '' : window.decodeURIComponent(hashChunks[1].replace(/\+/g, ' '));
+      }
+    }
+    return hashObj;
+  };
+
+  var HASH = URLHashParams();
+
+  if (!('URLHashParams' in window)) {
+    window.URLHashParams = URLHashParams;
+  }
+  if (!('HASH' in window)) {
+    window.HASH = HASH;
+  }
+
+  window.addEventListener('hashchange', function () {
+    HASH = URLHashParams();
+  });
+
+  var xhr = function (opts) {
     if (typeof opts === 'string') {
       opts = {
         url: opts
@@ -23,9 +60,9 @@
     opts.method = opts.method || 'get';
     if (typeof opts.data === 'object') {
       var formData = new FormData();
-      for (var prop in opts.data) {
+      Object.keys(opts.data).forEach(function (prop) {
         formData.append(prop, opts.data[prop]);
-      };
+      });
       opts.data = formData;
     }
     return new Promise(function (resolve, reject) {
@@ -37,43 +74,70 @@
       xhr.addEventListener('error', reject);
       xhr.send(opts.data);
     });
-  }
+  };
 
-  var commandLogs = document.querySelector('#command-logs');
+  var uploadMovie = function (movie) {
+    console.log(`download ${movie}`);
+  };
 
-  function logCommand (cmd) {
+  var uploadTVEpisode = function (show) {
+    logCommand('Latest show: ' + show);
+
+    show = encodeURI(show);
+
+    var fetchEp = function () {
+      return xhr({
+        method: 'get',
+        url: URLS.latestEpisode(show)
+      }).then(function (data) {
+        data = JSON.parse(data);
+        return data || {};
+      });
+    };
+
+    var displayEp = function (data) {
+      return new Promise(function (resolve, reject) {
+        data.voiceCommand = 'upload the latest episode of ' + decodeURI(show);
+        voiceUI.addEpInfo(data);
+        voiceUI.resultsShow();
+        resolve(data);
+      });
+    };
+
+    var addTransfer = function (data) {
+      return xhr({
+        method: 'post',
+        url: URLS.putioTransfersAdd(HASH.access_token),
+        data: {
+          url: data.magnetLink
+        }
+      }).then(function (data) {
+        // TODO: Do something with this return data.
+        logCommand('Add transfer data: ' + data);
+      });
+    };
+
+    var handleProblems = function (err) {
+      console.error(err);
+    };
+
+    return fetchEp()
+      .then(displayEp)
+      .then(addTransfer)
+      .catch(handleProblems);
+  };
+
+  var commandLogsEl = document.querySelector('#command-logs');
+  var logCommand = function (cmd) {
     console.log(cmd);
     var li = document.createElement('li');
     li.textContent = cmd;
-    commandLogs.appendChild(li);
-  }
-
-  var IS_PROD = !window.location.port && window.location.protocol === 'https:';
-
-  var API_DEV = 'http://localhost:7001/';
-  var API_PROD = 'https://popeye-api.herokuapp.com/';
-  var API = IS_PROD ? API_PROD : API_DEV;
-
-  var urls = {
-    putIO: {
-      base: 'https://api.put.io/v2',
-    },
-    popeyeAPI: {
-      latestEpisode: API_DEV + 'latestEpisode?show='
-    }
+    commandLogsEl.appendChild(li);
   };
 
-  // Generate the URL for the user to log in to Put.io and get redirected back to this page.
-  var redirectUri = encodeURIComponent(window.location.href);
-  var nextUri = encodeURIComponent(`${urls.putIO.base}/oauth2/authenticate?client_id=2801&response_type=token&redirect_uri=${redirectUri}`);
-  urls.putIO.login = `${urls.putIO.base}/oauth2/login?next=${nextUri}`;
-
-  // urls.putIO.filesList = `${urls.putIO.base}/files/list?parent_id=0&oauth_token=${hash.access_token}`;
-  urls.putIO.transfersAdd = `${urls.putIO.base}/transfers/add?oauth_token=${hash.access_token}`;
-
-  var logInBtn = document.querySelector('#log-in-btn');
-  if (logInBtn) {
-    logInBtn.setAttribute('href', urls.putIO.login);
+  var logInBtnEl = document.querySelector('#log-in-btn');
+  if (logInBtnEl) {
+    logInBtnEl.setAttribute('href', URLS.putioLogin);
   }
 
   if (!annyang) {
@@ -81,59 +145,17 @@
   }
 
   annyang.addCallback('soundstart', function () {
-    console.log('sound detected');
+    console.log('Speech detected');
   });
 
   annyang.addCallback('result', function () {
-    console.log('sound stopped');
+    console.log('Speech stopped');
   });
 
   var speechCommands = {
-    'upload (the) movie *movie': function (movie) {
-      console.log(`download ${movie}`);
-    },
-    'upload the latest episode of *show': function (show) {
-      show = encodeURI(show);
-      logCommand('Latest show: ' + show);
-
-      var fetchEp = function () {
-        return xhr({
-          method: 'get',
-          url: urls.popeyeAPI.latestEpisode + show
-        }).then(function (data) {
-          data = JSON.parse(data);
-          return data;
-        });
-      };
-
-      var displayEp = function (data) {
-        return new Promise(function (resolve, reject) {
-          data.voiceCommand = 'upload the latest episode of ' + decodeURI(show);
-          voiceUI.addEpInfo(data);
-          voiceUI.resultsShow();
-          resolve(data);
-        });
-      };
-
-      var addTransfer = function (data) {
-        return xhr({
-          method: 'POST',
-          url: urls.putIO.transfersAdd,
-          data: {
-            url: data.magnetLink
-          }
-        }).then(function (data) {
-          // TODO: Do something with this return data.
-          logCommand('Add transfer data: ' + data);
-        });
-      };
-
-      var reportProblems = function (fault) {
-        console.error(fault);
-      };
-
-      fetchEp().then(displayEp).then(addTransfer).catch(reportProblems);
-    }
+    'upload (the) movie *movie': uploadMovie,
+    'upload (the) latest episode of *show': uploadTVEpisode,
+    'hello': function () { alert('hello'); }
   };
 
   annyang.addCommands(speechCommands);
